9d049a1e65cffc81ae38a4c3fbbc242c77c35100
Muhammad Ajiz Alfarizi <muhammadajiz975@gmail.com>
2023-04-21 05:52:20 +0700

    📌 edit sidebar routing: /pertanyaan ke /diskusi

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -19,8 +19,8 @@ export default function SidebarClient(params) {
                     </Link>
                     <div className="">
                         <p className="mt-3 ml-10 text"><b>Menu</b></p>
-                        <Link href="/pertanyaan">
-                            <div className={`${router.pathname === '/pertanyaan' ? 'active' : '' } sidebar-btn`}>
+                        <Link href="/diskusi">
+                            <div className={`${router.pathname === '/diskusi' ? 'active' : '' } sidebar-btn`}>
                                 <span>
                                     <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-chat-square-dots" viewBox="0 0 16 16">
                                         <path d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1h-2.5a2 2 0 0 0-1.6.8L8 14.333 6.1 11.8a2 2 0 0 0-1.6-.8H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h2.5a1 1 0 0 1 .8.4l1.9 2.533a1 1 0 0 0 1.6 0l1.9-2.533a1 1 0 0 1 .8-.4H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
---
+++ src/pages/diskusi/buat.js
@@ -0,0 +1,7 @@
+export default function buat(params) {
+    return(
+        <>
+            <h1>Hello world</h1>
+        </>
+    )
+}
